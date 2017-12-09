751d96d5df6ea96440b9c626e35198730e4291dc
Tijmen Zwaan <tijmen.zwaan@gmail.com>
2017-12-09 16:09:38 +0100

    cleared blocks now dissappear in the correct order.

--- objects.js
+++ objects.js
@@ -624,6 +624,19 @@ function TaGame() {
                     chain = true;
             }
         }
+        this.combo.sort(function(a, b) {
+            if (a.y < b.y)
+                return 1;
+            if (a.y > b.y)
+                return -1;
+            if (a.y == b.y) {
+                if (a.x > b.x)
+                    return 1;
+                if (a.x < b.x)
+                    return -1;
+            }
+            return 0;
+        });
 
         combo = this.combo.length;
         while((block = this.combo.pop()) != undefined) {
